2257e232c292a878d620e755b2f93cdb7ba01fad
NikhilJHA01 <nik@Nikhils-MacBook-Air.local>
2023-01-19 20:47:45 +0530

    Added chat gtp code

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./App.css";
+import Options from "./components/Options";
+import { arrayItems } from "./AIOptions";
+import Translation from "./components/Translation";
+import { Configuration, OpenAIApi } from "openai";
+
+function App() {
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPEN_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const [option, setOption] = useState({});
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  console.log(import.meta.env.VITE_OPEN_API_KEY);
+  const selectOption = (option) => {
+    setOption(option);
+  };
+
+  const doStuff = async () => {
+    let object = { ...option, prompt: input };
+
+    const response = await openai.createCompletion(object);
+
+    setResult(response.data.choices[0].text);
+  };
+  console.log(option);
+
+  return (
+    <div className="App">
+      <h1>React AI APP</h1>
+      {!Object.values(option).length ? (
+        <Options arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation doStuff={doStuff} result={result} setInput={setInput} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Options.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Options = ({arrayItems,selectOption}) => {
+  return (
+
+      <div className="grid-main">
+        {arrayItems.map((item) => {
+          return (
+            <div
+              className="grid-child"
+              onClick={() => selectOption(item.option)}
+            >
+              <h3>{item.name}</h3>
+              <p>{item.description}</p>
+            </div>
+          );
+        })}
+      </div>
+  )
+}
+
+export default Options
